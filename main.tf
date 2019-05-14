51f7941ceac1f03f5f6f94047df7df3ff1caaf89
sharabinth <maha@hashicorp.com>
2019-05-14 14:16:14 +1000

    added network and security groups. Tested the creation of ec2 instance

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
 provider "aws" {
   # Use AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables
-  region     = "${var.aws_region}"
-  
+  region = "${var.aws_region}"
 }
 
-# Setup Network services such as VPC and also create a Security Group to enable SSH into the instance
-# 
-
+# Create the EC2 instance to install pTFE
 resource "aws_instance" "ptfe-demo" {
-  ami = "${var.amis}"
+  ami           = "${var.amis}"
   instance_type = "${var.instance_type}"
-  key_name = "${var.ssh_key_name}"
+  key_name      = "${var.ssh_key_name}"
+
+  # attach the security group
+  vpc_security_group_ids = ["${aws_security_group.sec_group.id}"]
+
+  # attach the subnet
+  subnet_id = "${aws_subnet.subnet.id}"
 
   # allocate atleast 40GB space for the pre-requisites
   root_block_device {
@@ -20,11 +23,12 @@ resource "aws_instance" "ptfe-demo" {
 
   # tags to name
   tags {
-    Name  = "${var.instance_name}"
+    Name  = "${var.resource_prefix_name}-demo"
     owner = "${var.owner}"
   }
 }
 
+# Create elastic IP and attach it to the EC2 instance
 resource "aws_eip" "ptfe-demo" {
   instance = "${aws_instance.ptfe-demo.id}"
 }
